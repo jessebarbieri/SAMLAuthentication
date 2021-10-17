24e8e84f4c635b669fbbe3390acdeb811f33da47
Jesse Barbieri <barbierijesse@gmail.com>
2021-10-17 09:53:04 -0400

    First Commit

---
+++ src/main/java/com/joblog/samlAuthentication/SamlAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.joblog.samlAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlAuthenticationApplication.class, args);
+	}
+
+}
